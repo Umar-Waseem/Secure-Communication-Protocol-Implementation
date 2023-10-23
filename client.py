d8676458b4e296fa6cd3acfb24e610fe1233fffc
Umar-Waseem <umar.waseem@gmail.com>
2023-10-23 23:47:03 +0500

    sent html from server and generate server key pair

--- client.py
+++ client.py
@@ -1,44 +1,50 @@
 import socket
+import webbrowser
+import os
 
-
+###################################
 ####### SOCKET CREATION ######
+###################################
 
-# lets create a socket for client server communication
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connecting to the server which lives on the localhost:12345
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
-###### SENDING CLIENT HELLO ###### (part 1 of 3 way handshake)
+###################################
+###### 1. SENDING CLIENT HELLO ######
+###################################
 
-# supported cipher suites
-cipher_suites = [
+supported_cipher_suites = [
     "TLS_RSA_WITH_AES_256_CBC_SHA256",
     "TLS_DHE_WITH_AES_256_CBC_SHA256",
     "TLS_RSA_WITH_AES_128_CBC_SHA256",
     "TLS_DHE_WITH_AES_128_CBC_SHA256"
 ]
-cipher_suites = ",".join(cipher_suites)
+supported_cipher_suites = ",".join(supported_cipher_suites)
 
-client_hello_message = cipher_suites
+client_hello_message = supported_cipher_suites
 print("(Client) ClientHello message to the server: ", client_hello_message)
-
-# convert the message to bytes
 client_hello_message = client_hello_message.encode()
-
-# Send the "ClientHello" message to the server
 client_socket.send(client_hello_message)
 
-# Receive the "ServerHello" message from the server
 server_hello_message = client_socket.recv(1024)
 server_hello_message = server_hello_message.decode()
 print("(Client) ServerHello message from the server: ", server_hello_message)
 
-# Close the connection
+# get request to server simple example which gives an html ui file
+client_socket.send("GET / HTTP/1.1\r\n".encode())
+data = client_socket.recv(1024)
+print(data.decode())
+
+# Save the HTML content to a local file
+with open("received_file.html", "wb") as html_file:
+    html_file.write(data)
+
+file_url = os.path.abspath("received_file.html")
+webbrowser.open("file://{}".format(file_url))
+
 client_socket.close()
-
-
 
 
 
--- server.py
+++ server.py
@@ -1,11 +1,59 @@
 import socket
 
-###### SOCKET CREATION ######
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
 
-# creating a socket for client server communication
+
+
+# generate public and private keys
+
+def get_public_private_keys():
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    public_key = private_key.public_key()
+
+    return private_key, public_key
+
+
+def save_public_key(public_key):
+    pem = public_key.public_bytes(
+        encoding=Encoding.PEM,
+        format=PublicFormat.SubjectPublicKeyInfo
+    )
+
+    with open("public_key.pem", "wb") as public_key_file:
+        public_key_file.write(pem)
+
+
+def save_private_key(private_key):
+    pem = private_key.private_bytes(
+        encoding=Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    with open("private_key.pem", "wb") as private_key_file:
+        private_key_file.write(pem)
+
+
+
+###################################
+###### SOCKET CREATION ######
+###################################
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind the socket to a specific address and port so server can listen to it
 server_address = ('localhost', 12345)
 server_socket.bind(server_address)
 
@@ -14,21 +62,19 @@ server_socket.listen(1)
 
 print("(Server) Waiting for a connection...")
 
-# accept a connection from the client
 client_socket, client_address = server_socket.accept()
 print("(Server) Connected to", client_address)
 
-###### SENDING SERVER HELLO ###### (part 2 of 3 way handshake)
+###################################
+###### 2. SENDING SERVER HELLO ###### 
+###################################
 
-# Receive the "ClientHello" message from the client
 client_hello_message = client_socket.recv(1024)
 print("(Server) ClientHello message from the client: ", client_hello_message)
 
-# convert client_hello_message to string
 client_hello_message = client_hello_message.decode()
 print("(Server) decoded client_hello_message: ", client_hello_message)
 
-# Extract supported cipher suites from the "ClientHello" message (for further processing) and create a list
 supported_cipher_suites = client_hello_message.split(",")
 print("(Server) Supported cipher suites: ", supported_cipher_suites)
 
@@ -36,12 +82,14 @@ print("(Server) Supported cipher suites: ", supported_cipher_suites)
 CHOSEN_CIPHER_SUITE = supported_cipher_suites[0]
 print("(Server) Server Chose cipher suite: ", CHOSEN_CIPHER_SUITE)
 
-# Create the "ServerHello" message
-server_hello_message = CHOSEN_CIPHER_SUITE  # Chosen cipher suite
-
-# Send the "ServerHello" message to the client
+server_hello_message = CHOSEN_CIPHER_SUITE 
 server_hello_message = server_hello_message.encode()
 client_socket.send(server_hello_message)
+
+# send the html file to client
+with open("index.html", "rb") as html_file:
+    data = html_file.read()
+    client_socket.send(data)
 
 # Close the connection
 client_socket.close()
